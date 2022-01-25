cdfbc0eace718673f9830778233f236926b3e51b
Eduardo Jaime <eduardojaime86@gmail.com>
2022-01-25 18:11:08 -0500

    Lesson06 database and filesystem access

---
+++ 202201/Lesson06/database/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+// Import SqlClient namespace: it contains SqlConnection, SqlCommand, SqlDataReader classes
+// In VSCode run command 'dotnet add package System.Data.SqlClient' on the terminal
+using System.Data.SqlClient;
+
+namespace database
+{
+    class Program
+    {
+        // global variable space
+        // Reference: https://www.connectionstrings.com/sql-server/
+        // Basic Standard Format: 
+        // Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password=myPassword;
+        // Somee.com Format:
+        const string connStr = "workstation id=ibtcollege.mssql.somee.com;packet size=4096;user id=ibtcollege_SQLLogin_1;pwd=dd69kxzi3m;data source=ibtcollege.mssql.somee.com;persist security info=False;initial catalog=ibtcollege";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            ShowNames();
+        }
+
+        public static void ShowNames()
+        {
+            // show all names in the students table
+            // ID-FirstName-LastName-EmailAddress
+            // ADO.NET > simplest way to connect to a SQL database
+
+            
+            // 1) Create connection object and open a connection to the DB
+            // Using > creates an object
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+
+                // 2) Create an empty command object
+                using(SqlCommand cmd = new SqlCommand())
+                {   
+                    // Associate connection and command text values
+                    cmd.Connection = conn;
+                    cmd.CommandText = "SELECT * FROM Students";
+
+                    // 3) Extract information using SqlDataReader
+                    SqlDataReader reader = cmd.ExecuteReader(); // execute reader returns a reader object containing all the results from the query
+                    // use a loop to access each record
+                    // read() returns true if there are more records to read
+                    while (reader.Read()) 
+                    {
+                        // access data by column name
+                        string fullName = reader["FirstName"] + " " + reader["LastName"];
+                        Console.WriteLine(fullName);
+                    }
+                }
+            }
+            // Cleanup > All objects created inside using statement are disposed (connections are closed)
+        }
+    }
+}
---
+++ 202201/Lesson06/filesystem/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+// Access file system
+using System.IO;
+
+namespace filesystem
+{
+    class Program
+    {
+        // specify a path to the file you want to write
+        const string PATH_TO_FILE = @"C:\file.txt";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            
+            // add two lines to the file
+            WriteToFile("Hello IBT Class!");
+            WriteToFile("We are programming!");
+
+            // call once since it will read the entire file
+            ReadFile();
+        }
+
+        // reads from a file and prints to the console  
+        public static void ReadFile()
+        {
+            // always check whether file exists
+            // Option 1
+            // if (File.Exists(PATH_TO_FILE) == false) {
+            //     File.Create(PATH_TO_FILE);
+            // }
+
+            // Option 2
+            FileInfo file = new FileInfo(PATH_TO_FILE);
+            if (file.Exists == false)
+            {
+                file.Create();
+            }
+
+            using (StreamReader reader = new StreamReader(PATH_TO_FILE))
+            {
+                string line = reader.ReadLine();
+                // if NOT null or empty then
+                while (!String.IsNullOrEmpty(line))
+                {
+                    Console.WriteLine(line);
+                    // read next line
+                    // it will return null when reader reaches end of file
+                    line = reader.ReadLine();
+                }
+                // ALWAYS CLOSE!!
+                reader.Close();
+            }
+        }
+
+        // appends text to a file
+        public static void WriteToFile(string text)
+        {
+            // always check whether file exists
+            // Option 1
+            if (File.Exists(PATH_TO_FILE) == false) {
+                File.Create(PATH_TO_FILE);
+            }
+
+            // Option 2
+            // FileInfo file = new FileInfo(PATH_TO_FILE);
+            // if (file.Exists == false)
+            // {
+            //     file.Create();
+            // }
+
+            // Use Directory or DirectoryInfo for directories
+
+            // use a stream class - ALWAYS CLOSE/DISPOSE STREAM CLASSES!
+            // set append to true to add to the file instead of destroying it every time
+            using (StreamWriter writer = new StreamWriter(PATH_TO_FILE, true))
+            {
+                writer.WriteLine(text);
+                // VERY IMPORTANT!!!
+                writer.Close();
+            }
+        }
+    }
+}
